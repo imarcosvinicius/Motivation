c60610f4a689f70efedb7d096b4a0e14a213610c
imarcosvinicius <imarcosvinicius@outlook.com>
2022-01-18 22:04:39 -0300

    Implemented capture of clicks and navigation between activities

--- app/src/main/java/com/example/motivation/SplashActivity.kt
+++ app/src/main/java/com/example/motivation/SplashActivity.kt
@@ -1,10 +1,13 @@
 package com.example.motivation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.motivation.databinding.ActivitySplashBinding
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivitySplashBinding
 
@@ -14,7 +17,24 @@ class SplashActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        binding.buttonSave.setOnClickListener(this)
 
+    }
 
+    override fun onClick(view: View) {
+        val id = view.id
+        if (id == R.id.buttonSave) {
+            handleSave()
+        }
+    }
+
+    private fun handleSave(){
+        val name = binding.editName.text.toString()
+
+        if (name != "") {
+            startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            Toast.makeText(this, getString(R.string.informe_seu_nome), Toast.LENGTH_SHORT).show()
+        }
     }
 }
