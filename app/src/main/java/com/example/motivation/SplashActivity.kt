f0739339869ae0f356168810072eff0a480c76ef
imarcosvinicius <imarcosvinicius@outlook.com>
2022-01-18 21:52:54 -0300

    Created the activity splash and defining the new main activity

---
+++ app/src/main/java/com/example/motivation/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.motivation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.motivation.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+
+    }
+}
