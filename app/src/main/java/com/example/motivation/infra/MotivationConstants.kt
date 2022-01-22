951079c7b0c508483e07dbe52c16cb2cd2dddebc
imarcosvinicius <imarcosvinicius@outlook.com>
2022-01-22 11:15:20 -0300

    Implementation capture clicks in main activity

--- app/src/main/java/com/example/motivation/infra/MotivationConstants.kt
+++ app/src/main/java/com/example/motivation/infra/MotivationConstants.kt
@@ -1,8 +1,15 @@
 package com.example.motivation.infra
 
-class MotivationConstants private constructor(){
+class MotivationConstants private constructor() {
 
     object KEY {
         val PERSON_NAME = "name"
+    }
+
+    object PHRASEFILTER {
+        val ALL = 1
+        val HAPPY = 2
+        val MORNING = 3
+
     }
 }
--- app/src/main/java/com/example/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/example/motivation/ui/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.motivation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.example.motivation.R
 import com.example.motivation.databinding.ActivityMainBinding
 import com.example.motivation.infra.MotivationConstants
 import com.example.motivation.infra.SecurityPreferences
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -21,5 +24,53 @@ class MainActivity : AppCompatActivity() {
         mSecurityPreference = SecurityPreferences(this)
 
         binding.textName.text = mSecurityPreference.getString(MotivationConstants.KEY.PERSON_NAME)
+
+        binding.imageAll.setOnClickListener(this)
+        binding.imageHappy.setOnClickListener(this)
+        binding.imageMorning.setOnClickListener(this)
+        binding.buttonNewPhrase.setOnClickListener(this)
+
     }
+
+    override fun onClick(view: View) {
+        val id = view.id
+
+        val listFilter = listOf(R.id.imageAll, R.id.imageHappy, R.id.imageMorning)
+
+        if (id == R.id.buttonNewPhrase) {
+            handleNewPhrase()
+        } else if (id in listFilter) {
+            handleFilter(id)
+        }
+    }
+
+    private fun handleFilter(id: Int) {
+
+        val imageAll = binding.imageAll
+        val imageHappy = binding.imageHappy
+        val imageMorning = binding.imageMorning
+
+        imageAll.setColorFilter(ContextCompat.getColor(this, R.color.white))
+        imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.white))
+        imageMorning.setColorFilter(ContextCompat.getColor(this, R.color.white))
+
+        when (id) {
+            R.id.imageAll -> {
+                imageAll.setColorFilter(ContextCompat.getColor(this, R.color.colorAccent))
+            }
+            R.id.imageHappy -> {
+                imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.colorAccent))
+            }
+            R.id.imageMorning -> {
+                imageMorning.setColorFilter(ContextCompat.getColor(this, R.color.colorAccent))
+            }
+        }
+
+
+    }
+
+    private fun handleNewPhrase() {
+
+    }
+
 }
